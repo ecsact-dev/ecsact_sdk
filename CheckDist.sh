4d7be656b9f214e0f4d2e7376e1c422da8e624ce
Ezekiel Warren <zekewarren@gmail.com>
2022-09-02 10:17:28 -0700

    Added CI

---
+++ CheckDist.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+bazel run --config=ci //:copy_dist_bin
+bazel run --config=ci //:copy_dist_include
+bazel run --config=ci //:copy_dist_codegen_plugins
+
+ls -R dist
+
+echo
+
+[ ! -d "dist/include/dist" ] && echo "Missing ecsact include directory" && exit 1
+[ ! -f "dist/include/bin/ecsact" ] && echo "Missing ecsact CLI" && exit 1
