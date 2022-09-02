7f51f107c40c632862d3acc32e61546fee8de16c
Ezekiel Warren <zekewarren@gmail.com>
2022-09-02 12:00:41 -0700

    end with exit 0?

--- CheckDist.sh
+++ CheckDist.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+bazel build --config=ci //:copy_dist_bin //:copy_dist_include //:copy_dist_codegen_plugins
+
 bazel run --config=ci //:copy_dist_bin
 bazel run --config=ci //:copy_dist_include
 bazel run --config=ci //:copy_dist_codegen_plugins
 
-ls -R dist
+find dist -type f
 
 echo
 
-[ ! -d "dist/include/dist" ] && echo "Missing ecsact include directory" && exit 1
-[ ! -f "dist/include/bin/ecsact" ] && echo "Missing ecsact CLI" && exit 1
+[ ! -d "dist/include/ecsact" ] && echo "Missing ecsact include directory" && exit 1
+[ ! -f "dist/bin/ecsact" ] && echo "Missing ecsact CLI" && exit 1
 
 exit 0
