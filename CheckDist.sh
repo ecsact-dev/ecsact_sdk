bd37ed799377307b4b375748fa81927a36372771
Ezekiel Warren <zekewarren@gmail.com>
2022-09-16 08:49:06 -0700

    Added missing runfiles for esact_rtb

--- CheckDist.sh
+++ CheckDist.sh
@@ -5,6 +5,7 @@ set -e
 bazel build --config=ci //:copy_dist_bin //:copy_dist_include //:copy_dist_codegen_plugins
 
 bazel run --config=ci //:copy_dist_bin
+bazel run --config=ci //:copy_runfiles
 bazel run --config=ci //:copy_dist_include
 bazel run --config=ci //:copy_dist_codegen_plugins
 
@@ -14,5 +15,7 @@ echo
 
 [ ! -d "dist/include/ecsact" ] && echo "Missing ecsact include directory" && exit 1
 [ ! -f "dist/bin/ecsact" ] && echo "Missing ecsact CLI" && exit 1
+[ ! -f "dist/bin/ecsact_rtb" ] && echo "Missing ecsact runtime builder CLI" && exit 1
+[ ! -d "dist/bin/ecsact_rtb.runfiles" ] && echo "Missing ecsact rtb runfiles" && exit 1
 
 exit 0
