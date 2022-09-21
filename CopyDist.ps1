a76a3dc338b0385dc878c698ca00eb63f5760c9c
Ezekiel Warren <zekewarren@gmail.com>
2022-09-21 16:17:54 -0700

    minor fix on pwsh scripts

--- CopyDist.ps1
+++ CopyDist.ps1
@@ -3,7 +3,8 @@
 $ErrorActionPreference = 'Stop'
 
 git clean dist -fdx
-bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress //:copy_dist_include
-bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress //:copy_runfiles
-bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress //:copy_dist_bin
-bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress //:copy_dist_codegen_plugins
+bazel build //:copy_dist_include //:copy_runfiles //:copy_dist_bin //:copy_dist_codegen_plugins
+bazel run --ui_event_filters=-info --noshow_progress //:copy_dist_include
+bazel run --ui_event_filters=-info --noshow_progress //:copy_runfiles
+bazel run --ui_event_filters=-info --noshow_progress //:copy_dist_bin
+bazel run --ui_event_filters=-info --noshow_progress //:copy_dist_codegen_plugins
