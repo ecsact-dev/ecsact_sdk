c018ee32d754449691da63288172b3c82c55dc3c
Ezekiel Warren <zekewarren@gmail.com>
2022-10-25 10:36:42 -0700

    app installer

--- UploadMsix.ps1
+++ UploadMsix.ps1
@@ -20,12 +20,15 @@ Expand-Archive -Path $ArchivePath -DestinationPath dist
 
 ((Get-Content -path .\dist\AppxManifest.xml -Raw) -replace '0.0.0.0-placeholder',"$($LatestTag).0") | Set-Content -Path .\dist\AppxManifest.xml
 
+((Get-Content -path .\ecsact_sdk.appinstaller -Raw) -replace '0.0.0.0-placeholder',"$($LatestTag)") | Set-Content -Path .\ecsact_sdk.appinstaller
+
 try {
 	$MsixPath = "ecsact_sdk_$($LatestTag)_windows_x64.msix"
 	$CertPassword = Read-Host "Cert Password"
 	MSIXHeroCLI pack -d dist -p $MsixPath
 	MSIXHeroCLI sign --file $env:USERPROFILE\Documents\Certificates\EcsactDev.pfx -p $CertPassword $MsixPath
-	gh release upload $LatestTag $MsixPath
+	gh release upload $LatestTag $MsixPath ecsact_sdk.appinstaller
 } finally {
 	git checkout .\dist\AppxManifest.xml
+	git checkout .\ecsact_sdk.appinstaller
 }
