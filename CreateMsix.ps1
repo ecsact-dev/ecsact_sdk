4f76b4ce73a57fb0f002dd55a848f1af8c2192f5
Ezekiel Warren <zekewarren@gmail.com>
2022-10-26 15:18:33 -0700

    Full publisher name and removed MSIXHero usage

--- CreateMsix.ps1
+++ CreateMsix.ps1
@@ -1,17 +1,49 @@
 #!/usr/bin/env pwsh
 
-param ([Parameter(Mandatory)] $Version, [Parameter(Mandatory)][SecureString] $CertPassword)
+param (
+	[Parameter(Mandatory)] $Version,
+	[Parameter(Mandatory)][SecureString] $CertPassword,
+	$CertPath = "$env:USERPROFILE\Documents\Certificates\EcsactDev.pfx"
+)
 
 $ErrorActionPreference = 'Stop'
 
+$MakeAppxPath = "${env:ProgramFiles(x86)}\Windows Kits\10\bin\10.0.22621.0\x64\makeappx.exe"
+$SignToolPath = "${env:ProgramFiles(x86)}\Windows Kits\10\bin\10.0.22621.0\x64\signtool.exe"
+
 $CertPasswordPlain = ConvertFrom-SecureString -SecureString $CertPassword -AsPlainText
-$CertPath = "$env:USERPROFILE\Documents\Certificates\EcsactDev.pfx"
 
 try {
 	((Get-Content -path .\dist\AppxManifest.xml -Raw) -replace '0.0.0.0-placeholder',"$($Version).0") | Set-Content -Path .\dist\AppxManifest.xml
 	$MsixPath = "ecsact_sdk_$($Version)_windows_x64.msix"
-	MSIXHeroCLI pack -d dist -p $MsixPath
-	MSIXHeroCLI sign --file $CertPath -p $CertPasswordPlain $MsixPath
+
+	& $SignToolPath sign `
+		/debug /fd SHA384 `
+		/f $CertPath `
+		/p $CertPasswordPlain `
+		/tr http://timestamp.sectigo.com /td SHA256 `
+		.\dist\bin\ecsact.exe `
+		.\dist\bin\ecsact_rtb.exe
+
+	if(-not $?) {
+		throw "Signing dist binaries failed with exit code ${LastExitCode}"
+	}
+
+	& $MakeAppxPath pack /v /o /h SHA384 /d .\dist\ /p $MsixPath
+
+	if(-not $?) {
+		throw "$MakeAppxPath exited with ${LastExitCode}"
+	}
+
+	& $SignToolPath sign `
+		/debug /fd SHA384 `
+		/f $CertPath `
+		/p $CertPasswordPlain `
+		/tr http://timestamp.sectigo.com /td SHA256 `
+		$MsixPath
+	if(-not $?) {
+		throw "Signing ${MsixPath} failed with exit code ${LastExitCode}"
+	}
 } finally {
 	git checkout .\dist\AppxManifest.xml
 }
--- UploadMsix.ps1
+++ UploadMsix.ps1
@@ -1,5 +1,10 @@
 #!/usr/bin/env pwsh
 
+param (
+	[Parameter(Mandatory)] $CertPath,
+	[Parameter(Mandatory)][SecureString] $CertPassword
+)
+
 $ErrorActionPreference = 'Stop'
 
 # GitHub requires TLS 1.2
@@ -18,18 +23,12 @@ git clean dist -dfx
 Write-Host "Extracting $ArchivePath to dist..."
 Expand-Archive -Path $ArchivePath -DestinationPath dist
 
-((Get-Content -path .\dist\AppxManifest.xml -Raw) -replace '0.0.0.0-placeholder',"$($LatestTag).0") | Set-Content -Path .\dist\AppxManifest.xml
-
 ((Get-Content -path .\ecsact_sdk.appinstaller -Raw) -replace '0.0.0.0-placeholder',"$($LatestTag).0") | Set-Content -Path .\ecsact_sdk.appinstaller
 ((Get-Content -path .\ecsact_sdk.appinstaller -Raw) -replace '0.0.0-placeholder',"$($LatestTag)") | Set-Content -Path .\ecsact_sdk.appinstaller
 
 try {
-	$MsixPath = "ecsact_sdk_$($LatestTag)_windows_x64.msix"
-	$CertPassword = Read-Host "Cert Password"
-	MSIXHeroCLI pack -d dist -p $MsixPath
-	MSIXHeroCLI sign --file $env:USERPROFILE\Documents\Certificates\EcsactDev.pfx -p $CertPassword $MsixPath
-	gh release upload $LatestTag $MsixPath ecsact_sdk.appinstaller
+	. .\CreateMsix.ps1 -CertPath $CertPath -Version $LatestTag -CertPassword $CertPassword
+	gh release upload $LatestTag $MsixPath ecsact_sdk.appinstaller --clobber
 } finally {
-	git checkout .\dist\AppxManifest.xml
 	git checkout .\ecsact_sdk.appinstaller
 }
