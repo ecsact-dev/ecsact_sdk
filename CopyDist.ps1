94be7756a7bc34b1328e873deb8177f82ae5ce1b
Ezekiel Warren <zekewarren@gmail.com>
2022-10-28 09:58:54 -0700

    new logo

--- CopyDist.ps1
+++ CopyDist.ps1
@@ -3,8 +3,9 @@
 $ErrorActionPreference = 'Stop'
 
 git clean dist -fdx
-bazel build //:copy_dist_include //:copy_runfiles //:copy_dist_bin //:copy_dist_codegen_plugins
+bazel build //:copy_dist_include //:copy_runfiles //:copy_dist_bin //:copy_dist_codegen_plugins //:copy_dist_images
 bazel run --ui_event_filters=-info --noshow_progress //:copy_dist_include
+bazel run --ui_event_filters=-info --noshow_progress //:copy_dist_images
 bazel run --ui_event_filters=-info --noshow_progress //:copy_runfiles
 bazel run --ui_event_filters=-info --noshow_progress //:copy_dist_bin
 bazel run --ui_event_filters=-info --noshow_progress //:copy_dist_codegen_plugins
