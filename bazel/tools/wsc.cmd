30fe9cde4618c897c6540e713abed26459103eda
Ezekiel Warren <zekewarren@gmail.com>
2023-03-28 20:29:51 -0700

    use GITHUB_REF if available (windows)

--- bazel/tools/wsc.cmd
+++ bazel/tools/wsc.cmd
@@ -1,9 +1,10 @@
 @echo off
 
 if "%GITHUB_REF%"=="" (
-	for /f %%i in ('git describe --tags --abbrev^=0') do set GIT_TAG=%%i
-	echo STABLE_ECSACT_SDK_VERSION %GIT_TAG%
-) else (
-	echo STABLE_ECSACT_SDK_VERSION %GITHUB_REF%
+	for /f %%i in ('git describe --tags --abbrev^=0') do (
+		set GITHUB_REF=%%i
+	)
 )
+
+echo STABLE_ECSACT_SDK_VERSION %GITHUB_REF%
 
