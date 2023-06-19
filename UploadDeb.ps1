1d49e542edf991f14088832ce70c9dd968ccedcd
Ezekiel Warren <zekewarren@gmail.com>
2023-06-19 13:03:50 -0700

    fix: wrong paths for deb package

--- UploadDeb.ps1
+++ UploadDeb.ps1
@@ -8,15 +8,15 @@ $ErrorActionPreference = 'Stop'
 $LatestTag = (gh release list -L 1).split()[0]
 Write-Host "Latest Tag: $LatestTag"
 
-$ArchiveUrl = "https://github.com/ecsact-dev/ecsact_sdk/releases/download/$($LatestTag)/ecsact_sdk_$($LatestTag)_linux_x64.zip"
-$ArchivePath = "ecsact_sdk_$($LatestTag)_linux_x64.zip"
+$ArchiveUrl = "https://github.com/ecsact-dev/ecsact_sdk/releases/download/$($LatestTag)/ecsact_sdk_$($LatestTag)_linux_x64.tar.gz"
+$ArchivePath = "ecsact_sdk_$($LatestTag)_linux_x64.tar.gz"
 
 Write-Host "Downloading $ArchiveUrl to $ArchivePath..."
 Invoke-WebRequest $ArchiveUrl -OutFile $ArchivePath -UseBasicParsing
 
 git clean dist -dfx
 Write-Host "Extracting $ArchivePath to dist..."
-Expand-Archive -Path $ArchivePath -DestinationPath dist
+tar -xf $ArchivePath -C dist
 
 . .\CreateDeb.ps1 -Version $LatestTag
 gh release upload $LatestTag $DebPath --clobber
