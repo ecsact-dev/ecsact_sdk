1c2407338db0bfea8669054f7b289b55b030838b
Ezekiel Warren <zekewarren@gmail.com>
2023-06-19 12:57:21 -0700

    feat: deb upload script

---
+++ UploadDeb.ps1
@@ -0,0 +1,23 @@
+#!/usr/bin/env pwsh
+
+$ErrorActionPreference = 'Stop'
+
+# GitHub requires TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$LatestTag = (gh release list -L 1).split()[0]
+Write-Host "Latest Tag: $LatestTag"
+
+$ArchiveUrl = "https://github.com/ecsact-dev/ecsact_sdk/releases/download/$($LatestTag)/ecsact_sdk_$($LatestTag)_linux_x64.zip"
+$ArchivePath = "ecsact_sdk_$($LatestTag)_linux_x64.zip"
+
+Write-Host "Downloading $ArchiveUrl to $ArchivePath..."
+Invoke-WebRequest $ArchiveUrl -OutFile $ArchivePath -UseBasicParsing
+
+git clean dist -dfx
+Write-Host "Extracting $ArchivePath to dist..."
+Expand-Archive -Path $ArchivePath -DestinationPath dist
+
+. .\CreateDeb.ps1 -Version $LatestTag
+gh release upload $LatestTag $DebPath --clobber
+
