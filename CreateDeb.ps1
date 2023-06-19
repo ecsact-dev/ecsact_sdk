7d644c53953fc212e5631e35d7cc83208f71eda6
Ezekiel Warren <zaucy@users.noreply.github.com>
2023-06-19 15:25:32 -0700

    feat: deb package scripts (#271)

---
+++ CreateDeb.ps1
@@ -0,0 +1,21 @@
+#!/usr/bin/env pwsh
+
+param (
+	[Parameter(Mandatory)] $Version
+)
+
+$ErrorActionPreference = 'Stop'
+
+$DebPath = "ecsact_sdk_$($Version)_amd64.deb"
+
+try {
+	((Get-Content -path ./dist/DEBIAN/control -Raw) -replace '0.0.0-placeholder',"$($Version)") | Set-Content -Path ./dist/DEBIAN/control
+	rm -rf ./dist/images
+	rm ./dist/AppxManifest.xml
+	rm -f ./dist/bin/ecsact_rtb.runfiles/ecsact_cli/ecsact
+	dpkg-deb --build --root-owner-group ./dist $DebPath
+} finally {
+	git checkout ./dist/DEBIAN/control
+	git checkout ./dist/AppxManifest.xml
+}
+
