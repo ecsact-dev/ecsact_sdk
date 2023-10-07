e86b31e4681f7a076346f97788aa3fb6ec674cb8
Ezekiel Warren <zaucy@users.noreply.github.com>
2023-10-06 19:17:26 -0700

    fix: strip ref/tags/ from version string (#301)

--- bazel/tools/wsc.sh
+++ bazel/tools/wsc.sh
@@ -5,6 +5,6 @@ set -e
 if [[ -z "${GITHUB_REF}" ]]; then
 	echo "STABLE_ECSACT_SDK_VERSION $(git describe --tags --abbrev=0)"
 else
-	echo "STABLE_ECSACT_SDK_VERSION ${GITHUB_REF}"
+	echo "STABLE_ECSACT_SDK_VERSION ${GITHUB_REF##*/}"
 fi
 
