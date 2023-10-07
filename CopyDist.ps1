aab68482bc74692fc7a190158eb0421e76ff4fa2
Ezekiel Warren <zekewarren@gmail.com>
2023-10-06 17:27:43 -0700

    feat: bzlmodify + update ecsact modules

--- CopyDist.ps1
+++ CopyDist.ps1
@@ -3,9 +3,8 @@
 $ErrorActionPreference = 'Stop'
 
 git clean dist -fdx
-bazel build //:copy_dist_include //:copy_runfiles //:copy_dist_bin //:copy_dist_codegen_plugins //:copy_dist_images
+bazel build //:copy_dist_include //:copy_dist_bin //:copy_dist_codegen_plugins //:copy_dist_images
 bazel run --ui_event_filters=-info --noshow_progress //:copy_dist_include
 bazel run --ui_event_filters=-info --noshow_progress //:copy_dist_images
-bazel run --ui_event_filters=-info --noshow_progress //:copy_runfiles
 bazel run --ui_event_filters=-info --noshow_progress //:copy_dist_bin
 bazel run --ui_event_filters=-info --noshow_progress //:copy_dist_codegen_plugins
--- bazel/copts.bzl
+++
@@ -1,4 +0,0 @@
-copts = select({
-    "@bazel_tools//tools/cpp:msvc": ["/std:c++20", "/Zc:preprocessor", "/permissive-"],
-    "//conditions:default": ["-std=c++20", "-Werror", "-fexperimental-library"],
-})
