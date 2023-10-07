c6bbfc3c6b2c73ca69f47bdadf4e9bf0c6d9aa37
Ezekiel Warren <zekewarren@gmail.com>
2023-10-06 18:50:08 -0700

    fix: add missing flag for linux

--- CheckDist.sh
+++ CheckDist.sh
@@ -4,7 +4,6 @@ set -e
 
 bazel build --config=ci //:copy_runfiles //:copy_dist_bin //:copy_dist_include //:copy_dist_codegen_plugins
 
-bazel run --config=ci //:copy_runfiles
 bazel run --config=ci //:copy_dist_bin
 bazel run --config=ci //:copy_dist_include
 bazel run --config=ci //:copy_dist_codegen_plugins
@@ -15,7 +14,6 @@ echo
 
 [ ! -d "dist/include/ecsact" ] && echo "Missing ecsact include directory" && exit 1
 [ ! -f "dist/bin/ecsact" ] && echo "Missing ecsact CLI" && exit 1
-[ ! -f "dist/bin/ecsact_rtb" ] && echo "Missing ecsact runtime builder CLI" && exit 1
-[ ! -d "dist/bin/ecsact_rtb.runfiles" ] && echo "Missing ecsact rtb runfiles" && exit 1
+[ ! -f "dist/bin/ecsact_lsp_server" ] && echo "Missing ecsact lsp CLI" && exit 1
 
 exit 0
